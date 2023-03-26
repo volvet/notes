7471c807780ae3072877f49f0c99b60d10eeb0b2
volvet <volvet@163.com>
2023-03-26 19:25:34 +0800

    transformer

--- src/transformer/transformer.py
+++ src/transformer/transformer.py
@@ -124,23 +124,68 @@ class PositionalEncoding(nn.Module):
     return torch.FloatTensor(sinusoid_table).unsqueeze(0)
     
 class Encoder(nn.Module):
-  def __init__(self):
-    pass
+  def __init__(self, n_src_vocab, d_word_vec, n_layers, n_head, d_k, d_v, d_model, d_inner, pad_idx, dropout=0.1, n_position=200, scale_emb=False):
+    super().__init__()
+    self.src_word_emb = nn.Embedding(n_src_vocab, d_word_vec, padding_idx=pad_idx)
+    self.position_enc = PositionalEncoding(d_word_vec, n_position=n_position)
+    self.dropout = nn.Dropout(dropout)
+    self.layer_stack = nn.ModuleList([EncoderLayer(d_model, d_inner, n_head, d_k, d_v, dropout=dropout) for _ in range(n_layers)])
+    self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
+    self.scale_emb = scale_emb
+    self.d_model = d_model
   
-  def forward(self):
-    pass
+  def forward(self, src_seq, src_mask, return_attns=False):
+    enc_slf_attn_list = []
+    enc_output = self.src_word_emb(src_seq)
+    if self.scale_emb:
+      enc_output *= self.d_model ** 0.5
+    enc_output = self.dropout(self.position_enc(enc_output))
+    enc_output = self.layer_norm(enc_output)
+    for enc_layer in self.layer_stack:
+      enc_output, enc_slf_attn = enc_layer(enc_output, slf_attn_mask=src_mask)
+      enc_slf_attn_list += [enc_slf_attn] if return_attns else []
+
+    if return_attns:
+      return enc_output, enc_slf_attn_list
+    else:
+      return enc_output
 
 class Decoder(nn.Module):
+  def __init__(self, n_trg_vocab, d_word_vec, n_layers, n_head, d_k, d_v, d_model, d_inner, pad_idx, n_position=200, dropout=0.1, scale_emb=False):
+    super().__init_()
+    self.trg_word_emb = nn.Embedding(n_trg_vocab, d_word_vec, padding_idx=pad_idx)
+    self.position_enc = PositionalEncoding(d_word_vec, n_position=n_position)
+    self.dropout = nn.Dropout(dropout)
+    self.layer_stack = nn.ModuleList([
+      DecoderLayer(d_model, d_inner, n_head, d_k, d_v) for _ in range(n_layers)])
+    self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
+    self.scale_emb = scale_emb
+    self.d_model = d_model
+
+  def forward(self, trg_seq, trg_mask, enc_output, src_mask, return_attns=False):
+    dec_slf_attn_list, dec_enc_attn_list = [], []
+    dec_output = self.trg_word_emb(trg_seq)
+    if self.scale_emb:
+      dec_output *= self.d_model ** 0.5
+    dec_output = self.dropout(dec_output)
+    dec_output = self.layer_norm(dec_output)
+    for dec_layer in self.layer_stack:
+      dec_output, dec_slf_attn, dec_enc_attn = dec_layer(dec_output, enc_output, slf_attn_mask=trg_mask, dec_enc_attn_mask=src_mask)
+      dec_slf_attn_list += [dec_slf_attn] if return_attns else []
+      dec_enc_attn_list += [dec_enc_attn] if return_attns else []
+
+    if return_attns:
+      return dec_output, dec_slf_attn_list, dec_enc_attn_list
+    else:
+      return dec_output
+
+class Transformer(nn.Module):
   def __init__(self):
     pass
   
   def forward(self):
     pass
 
-
-def get_position_angle_vec(position):
-  d_hid = 256
-  return [position/np.power(10000, 2*(hid_j//2)/d_hid) for hid_j in range(d_hid)]
 
 if __name__ == '__main__':
   print('Hello, transformer!')
